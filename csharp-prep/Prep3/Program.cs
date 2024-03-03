346891d3af54118413ac1f368b0c089664596b75
Jared Cook <jpcook98@gmail.com>
2024-03-02 23:18:39 -0700

    Wrote Prep 3 code

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Formats.Asn1;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response = ("yes");
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            int number = 9999;
+
+            while (magicNumber != number)
+            {
+                Console.Write("What is the magic number? ");
+                string userInput = Console.ReadLine();
+                number = int.Parse(userInput);
+                if (magicNumber > number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You Guessed it!!");
+                }
+            }
+            Console.Write("Do you want to continue? ");
+            response = Console.ReadLine();
+        } while (response == "yes");
     }
 }
